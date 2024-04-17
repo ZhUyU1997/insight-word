809ca9393dfd71afadc44beb6990e48b2a4223da
Yu Zhu <891085309@qq.com>
2024-04-17 23:25:42 +0800

    v0.9.0

--- tools/release.ts
+++ tools/release.ts
@@ -6,12 +6,16 @@ import { fileURLToPath } from "url";
 import { $ } from "execa";
 import prompts from "prompts";
 
+const $$ = $({ stdio: "inherit" });
+
 async function release() {
   const __dirname = dirname(fileURLToPath(import.meta.url));
   const v = semver.parse(pkg.version)!;
   const version = semver.inc(pkg.version, v.minor >= 9 ? "major" : "minor")!;
 
-  await $`git diff --cached --stat`;
+  console.log("需要提交的暂存文件如下：");
+
+  await $$`git diff --cached --stat`;
 
   const response = await prompts({
     type: "confirm",
@@ -25,11 +29,11 @@ async function release() {
   fs.writeJSONSync(path.join(__dirname, "..", "package.json"), pkg, {
     spaces: "  ",
   });
-  await $`git add package.json`;
-  await $`git commit -m v${version}`;
-  await $`git tag v${version}`;
-  await $`git push`;
-  await $`git push origin v${version}`;
+  await $$`git add package.json`;
+  await $$`git commit -m v${version}`;
+  await $$`git tag v${version}`;
+  await $$`git push`;
+  await $$`git push origin v${version}`;
 }
 
 release();
