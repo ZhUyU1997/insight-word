ed525f130de0fd7390d48110c8fea669d2c398d9
Yu Zhu <891085309@qq.com>
2024-04-17 23:21:32 +0800

    v0.8.0

---
+++ tools/release.ts
@@ -0,0 +1,35 @@
+import semver from "semver";
+import pkg from "../package.json";
+import fs from "fs-extra";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { $ } from "execa";
+import prompts from "prompts";
+
+async function release() {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const v = semver.parse(pkg.version)!;
+  const version = semver.inc(pkg.version, v.minor >= 9 ? "major" : "minor")!;
+
+  await $`git diff --cached --stat`;
+
+  const response = await prompts({
+    type: "confirm",
+    name: "confirm",
+    message: `请确认是否提交v${version}?`,
+  });
+
+  if (!response.confirm) process.exit();
+
+  pkg.version = version;
+  fs.writeJSONSync(path.join(__dirname, "..", "package.json"), pkg, {
+    spaces: "  ",
+  });
+  await $`git add package.json`;
+  await $`git commit -m v${version}`;
+  await $`git tag v${version}`;
+  await $`git push`;
+  await $`git push origin v${version}`;
+}
+
+release();
